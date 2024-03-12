2be2a34331e66ef82312b1b286ea603b3d4e3732
Henry-Hiles <henry@henryhiles.com>
2024-03-12 14:02:50 -0400

    Add config to package

--- package.ts
+++ package.ts
@@ -5,9 +5,15 @@ export class Package extends Data {
     name: string
     commit: string
     origin: string
+    config: Record<string, string>
 
     withCommit(commit: string): Package {
-        // @ts-ignore
+        // @ts-expect-error
         return this.copy({ commit: commit })
+    }
+
+    withConfig(config: Record<string, string>): Package {
+        // @ts-expect-error
+        return this.copy({ config: config })
     }
 }
